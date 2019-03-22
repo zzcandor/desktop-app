4db9b41522fe558aff992fdf9758c86e419e6c65
zhaitao <370985116@qq.com>
2019-03-22 20:11:41 +0800

    RecyclerView

--- src/main.js
+++ src/main.js
@@ -16,6 +16,7 @@ import blaze from '@/blaze/blaze'
 import i18n from '@/utils/i18n.js'
 import { faArrowLeft, faArrowRight, faChevronDown, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { faPaperPlane } from '@fortawesome/free-regular-svg-icons'
+import RecyclerView from 'vue-recyclerview'
 
 const fontawesome = require('@fortawesome/vue-fontawesome')
 library.add(faArrowLeft, faArrowRight, faChevronDown, faSearch, faPaperPlane)
@@ -28,6 +29,7 @@ Vue.use(Dialog)
 Vue.use(Menu)
 Vue.use(Toast)
 Vue.use(ImageViewer)
+Vue.use(RecyclerView)
 
 Vue.axios.defaults.baseURL = 'https://api.mixin.one/'
 Vue.axios.defaults.headers.post['Content-Type'] = 'application/json'
